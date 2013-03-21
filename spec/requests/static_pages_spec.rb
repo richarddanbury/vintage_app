3e944b291b9b2709892bad9054f5c6e49a18ab16
richarddanbury <richard@salesmaster.co.uk>
2013-03-21 15:44:59 +0000

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  # describe "GET /static_pages" do
+  #   it "works! (now write some real specs)" do
+  #     # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+  #     get static_pages_index_path
+  #     response.status.should be(200)
+
+  describe "Home page" do
+    it "should have the content 'Vintage App'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Vintage App')
+    end
+
+    it "should have the right title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Vintage App | Home")
+    end
+  end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the right title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "Vintage App | Help")
+    end
+  end
+
+    describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the right title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "Vintage App | About Us")
+    end
+  end
+end
