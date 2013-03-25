fe88ee7c2e88acf0b4ece7ba84bdb21a98daaee3
richarddanbury <richard@salesmaster.co.uk>
2013-03-25 16:29:46 +0000

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   #Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Vintage App"
+    base_title = "Little Vintage Emporium App"
     if page_title.empty?
       base_title
     else
